1e036688b16fe97fd61a47e097d9c4637545dd4a
Niklas Korz <niklas@niklaskorz.de>
2023-04-21 17:15:02 +0200

    Initial Commit

---
+++ Mucoce/MucoceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MucoceApp.swift
+//  Mucoce
+//
+//  Created by Niklas Korz on 21.04.23.
+//
+
+import SwiftUI
+
+@main
+struct MucoceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
